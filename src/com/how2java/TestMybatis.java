f5838d4c06645063ff9effa45470c513a2858d66
lenovo <lenovo@DESKTOP-DOBF7UR>
2019-08-21 21:00:27 +0800

    mybatis4 test

---
+++ src/com/how2java/TestMybatis.java
@@ -0,0 +1,41 @@
+package com.how2java;
+  
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+ 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+ 
+import com.how2java.pojo.Order;
+import com.how2java.pojo.OrderItem;
+import com.how2java.pojo.Product;
+  
+public class TestMybatis {
+  
+    public static void main(String[] args) throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession session = sqlSessionFactory.openSession();
+  
+        listOrder(session);
+ 
+        session.commit();
+        session.close();
+  
+    }
+ 
+    private static void listOrder(SqlSession session) {
+        List<Order> os = session.selectList("listOrder");
+        for (Order o : os) {
+            System.out.println(o.getCode());
+            List<OrderItem> ois= o.getOrderItems();
+            for (OrderItem oi : ois) {
+                System.out.format("\t%s\t%f\t%d%n", oi.getProduct().getName(),oi.getProduct().getPrice(),oi.getNumber());
+            }
+        }
+    }
+}
---
+++ src/com/how2java/TestMybatis2.java
@@ -0,0 +1,63 @@
+package com.how2java;
+  
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+ 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+ 
+import com.how2java.pojo.Order;
+import com.how2java.pojo.OrderItem;
+import com.how2java.pojo.Product;
+  
+public class TestMybatis2 {
+  
+    public static void main(String[] args) throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession session = sqlSessionFactory.openSession();
+  
+        deleteOrderItem(session);
+//        addOrderItem(session);
+        listOrder(session);
+ 
+        session.commit();
+        session.close();
+  
+    }
+ 
+    private static void deleteOrderItem(SqlSession session) {
+        Order o1 = session.selectOne("getOrder",1);
+        Product p6 = session.selectOne("getProduct",6);
+        OrderItem oi = new OrderItem();
+        oi.setProduct(p6);
+        oi.setOrder(o1);
+        session.delete("deleteOrderItem", oi);     
+    }
+ 
+    private static void addOrderItem(SqlSession session) {
+        Order o1 = session.selectOne("getOrder",1);
+        Product p6 = session.selectOne("getProduct",6);
+        OrderItem oi = new OrderItem();
+        oi.setProduct(p6);
+        oi.setOrder(o1);
+        oi.setNumber(200);
+         
+        session.insert("addOrderItem", oi);
+    }
+ 
+    private static void listOrder(SqlSession session) {
+        List<Order> os = session.selectList("listOrder");
+        for (Order o : os) {
+            System.out.println(o.getCode());
+            List<OrderItem> ois= o.getOrderItems();
+            for (OrderItem oi : ois) {
+                System.out.format("\t%s\t%f\t%d%n", oi.getProduct().getName(),oi.getProduct().getPrice(),oi.getNumber());
+            }
+        }
+    }
+}
